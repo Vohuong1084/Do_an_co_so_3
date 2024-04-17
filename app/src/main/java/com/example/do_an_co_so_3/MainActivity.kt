687dc0863a3696722fc5cf8423e95b8b36e443b1
NghiLam1106 <lam91391@gmail.com>
2024-04-17 19:14:56 +0700

    first commit

---
+++ app/src/main/java/com/example/do_an_co_so_3/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.do_an_co_so_3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.do_an_co_so_3.ui.theme.Do_an_co_so_3Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Do_an_co_so_3Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Do_an_co_so_3Theme {
+
+    }
+}
