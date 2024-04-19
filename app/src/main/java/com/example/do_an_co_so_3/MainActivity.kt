52a5014000a51bf3119d3230a233e650f4ff152c
Vohuong1084 <thihuongvo699@gmail.com>
2024-04-19 20:18:33 +0700

    test

--- app/src/main/java/com/example/do_an_co_so_3/MainActivity.kt
+++ app/src/main/java/com/example/do_an_co_so_3/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    Text(text = "Helo")
                 }
             }
         }
